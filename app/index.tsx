1d5259c118f15fd5e64bcc39a899590af13d39a5
Andriy Suprunenko <suprunenko.andriy@gmail.com>
2025-09-24 20:44:22 +0300

    try some thinks

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World!</Text>
+    <View>
+      <Text style={styles.title}>Hello World!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+  },
+});
